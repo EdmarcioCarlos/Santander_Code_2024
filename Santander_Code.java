90b8db5b488f7963cc15275068d0ea4a20f0e848
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2024-06-11 08:46:49 -0300

    Santander_Code_Inicio
    Criação do primeiro projeto

---
+++ Santander_Code.java
@@ -0,0 +1,9 @@
+public class Santander_Code {
+
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+
+        int numero = 2;
+        System.out.println(numero);
+    }
+}
